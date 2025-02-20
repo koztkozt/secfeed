535e482bc5a2af118115bc4df10f93cf2700ae0d
ztko <96878659+koztkozt@users.noreply.github.com>
2025-02-20 18:02:10 +0800

    fix headers

--- secfeed.py
+++ secfeed.py
@@ -14,7 +14,22 @@ from rocketchat_API.rocketchat import RocketChat
 DB_PATH = "secfeed.db"
 LIST_PARSED_DATA = []
 USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
-HEADERS = {"User-Agent": USER_AGENT}
+HEADERS = {
+    "User-Agent": USER_AGENT,
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
+    "Accept-Language": "en-US,en;q=0.9",
+    "Accept-Encoding": "gzip, deflate, br",
+    "Connection": "keep-alive",
+    "Upgrade-Insecure-Requests": "1",
+    "sec-ch-ua": '"Chromium";v="112", "Google Chrome";v="112"',
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": '"Windows"',
+    "Sec-Fetch-Dest": "document",
+    "Sec-Fetch-Mode": "navigate",
+    "Sec-Fetch-Site": "none",
+    "Sec-Fetch-User": "?1",
+    "Cache-Control": "max-age=0"
+}
 
 # Set the webhook URL
 webhook_url = "https://your-rocketchat-webhook-url.com"
